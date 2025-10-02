f70155db111b5cc6a02914dd9826bd40dcdb0e0b
Nagasai Kandhukuri <nagasaikandhukuri@Nagasais-MacBook-Air.local>
2025-10-02 17:18:46 +0530

    chore: remove out directory

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,12 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export',
+
+
+  
+  // output: 'export', // because we want Vercel to build and serve dynamically
+  
+  
   typescript: {
     ignoreBuildErrors: true,
   },
